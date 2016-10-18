f46f0ea8ddd454f5e9a6ae0eb48590ef2b4a69f3
tanmoy majumdar <tanmoy.majumdar@kaz.com.bd>
2016-10-18 19:25:52 +0600

    code update

--- src/main/java/threading/Java8Concurrency.java
+++ src/main/java/threading/Java8Concurrency.java
@@ -1,9 +1,7 @@
 package threading;
 
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class Java8Concurrency {
 
@@ -36,12 +34,43 @@ public class Java8Concurrency {
         thread.start();
     }
 
-    public static void three() {
+    public static void three() throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newSingleThreadExecutor();
         executor.submit(() -> {
             String threadName = Thread.currentThread().getName();
             System.out.println("Hello " + threadName);
         });
+        try {
+            System.out.println("attempt to shutdown executor");
+            executor.shutdown();
+            executor.awaitTermination(5, TimeUnit.SECONDS);
+        }
+        catch (InterruptedException e) {
+            System.err.println("tasks interrupted");
+        }
+        finally {
+            if (!executor.isTerminated()) {
+                System.err.println("cancel non-finished tasks");
+            }
+            executor.shutdownNow();
+            System.out.println("shutdown finished");
+        }
+
+        Callable<Integer> task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                return 123;
+            }
+            catch (InterruptedException e) {
+                throw new IllegalStateException("task interrupted", e);
+            }
+        };
+        ExecutorService executor2 = Executors.newFixedThreadPool(1);
+        Future<Integer> future = executor2.submit(task);
+        System.out.println("future done? " + future.isDone());
+        Integer result = future.get();
+        System.out.println("future done? " + future.isDone());
+        System.out.print("result: " + result);
     }
 
 
