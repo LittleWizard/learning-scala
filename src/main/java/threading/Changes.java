77188b5cb27ed806816bcddf3c713795ed5cf82c
tanmoy majumdar <tanmoy.majumdar@kaz.com.bd>
2016-10-25 13:46:17 +0600

    nio2 example

---
+++ src/main/java/threading/Changes.java
@@ -0,0 +1,67 @@
+package threading;
+
+
+import javax.naming.ConfigurationException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Changes {
+
+
+    public static void nio2One() {
+        try
+        {
+            //do sbt package first
+            Path file = Paths.get("target/scala-2.11/learning-scala_2.11-1.0-SNAPSHOT.jar");
+            AsynchronousFileChannel channel = AsynchronousFileChannel.open(file);
+            ByteBuffer buffer = ByteBuffer.allocate(100_000);
+            Future<Integer> result = channel.read(buffer, 0);
+            while(!result.isDone())
+            {
+               System.out.println("working in main thread");
+            }
+            Integer bytesRead = result.get();
+            System.out.println("Bytes read [" + bytesRead + "]");
+        }
+        catch (IOException | ExecutionException | InterruptedException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    public static void exampleTryWith() {
+        String fileName = "someFile.bin";
+        try(FileInputStream fin = new FileInputStream(fileName);
+            ObjectInputStream in = new ObjectInputStream(fin)) {
+
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Config file '" + fileName + "' is missing or malformed");
+        } catch (IOException iox) {
+            System.err.println("Error while processing file '" + fileName + "'");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+
+        nio2One();
+
+
+    }
+
+}
